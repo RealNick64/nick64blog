76f08d0c27fa1bdf55c6fe64bf8a5dac32a65507
RealNick64 <nicholasdanilet@gmail.com>
2024-07-30 14:20:03 -0700

    lkasdfjnsdlfjknsgdf

---
+++ content/netlogs/2024/07/video-netlog-11/index.md
@@ -0,0 +1,11 @@
+---
+title: "Video Netlog 11: Being Conflicted on Popularity"
+description: "I talk about a topic that has no relation to me at all (being popular but not wanting to be popular at the same time)."
+image: "/netlogs/2024/07/video-netlog-11/images/thumb.jpg"
+date: 2024-07-30T14:09:53-07:00
+draft: false
+tags: ["videos"]
+---
+I talk about a topic that has no relation to me at all (being popular but not wanting to be popular at the same time).
+
+{{< youtube VNnEm7Mukus >}}
---
+++ themes/Simple/static/js/quotes.js
@@ -0,0 +1,17 @@
+var quotes = [
+    "Underidoderidoderiododeridoo <br> - Winston Churchill",
+    "the j",
+    "nice penis bro, did your mom make it for you",
+    "I was getting thirsty so I had a sip of soda <br> It was too late when i realized it was scary soda"
+]
+
+var author = [
+    "- Winston Churchill",
+    "- Bill Clinton",
+    "- Nick64",
+    "- Unknown Reddit User"
+]
+
+window.onload = function newQuote() {
+    document.getElementById('quote').innerHTML = "Underidoderidoderiododeridoo <br> - Winston Churchill";
+}
